9bd5e9437dd29f4bad08f16fe5691ce2af56decc
aiguibin <aiguibin@163.com>
2019-09-02 22:54:45 +0800

    重构代码

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                // 用正则去匹配要用该 loader 转换的 CSS 文件
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader?minimize'],
+            }
+        ]
+    }
+};
